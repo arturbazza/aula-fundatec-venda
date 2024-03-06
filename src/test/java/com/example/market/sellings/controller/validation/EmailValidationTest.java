33dd73a828b36cf0ebd4bcb412305bfa72aaa459
Giovanni Silva <no-reply@gsilva.pro>
2024-03-06 20:31:03 -0300

    added test with method source

--- src/test/java/com/example/market/sellings/controller/validation/EmailValidationTest.java
+++ src/test/java/com/example/market/sellings/controller/validation/EmailValidationTest.java
@@ -3,7 +3,11 @@ package com.example.market.sellings.controller.validation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 class EmailValidationTest {
 
@@ -22,4 +26,22 @@ class EmailValidationTest {
         boolean result = emailValidation.validate(email);
         Assertions.assertFalse(result);
     }
+
+    @ParameterizedTest
+    @MethodSource(value = "emailsFactory")
+    void invalidEmailWithMethodSource(String email, boolean isValid) {
+        EmailValidation emailValidation = new EmailValidation();
+        boolean result = emailValidation.validate(email);
+        Assertions.assertEquals(result, isValid);
+    }
+
+    public static Stream<Arguments> emailsFactory() {
+        return Stream.of(
+                Arguments.of("teste@.com", false),
+                Arguments.of("teste@teste.com", true),
+                Arguments.of("aline@gmail.com", true)
+        );
+    }
+
+
 }
